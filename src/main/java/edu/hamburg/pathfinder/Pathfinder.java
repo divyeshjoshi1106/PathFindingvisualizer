a78193b1f90496663ff57c5d0c58de2585064aa7
Yuriel849 <yuriel849sp@gmail.com>
2021-03-02 00:21:56 +0100

    Pathfinder interface

---
+++ src/main/java/edu/hamburg/pathfinder/Pathfinder.java
@@ -0,0 +1,19 @@
+package edu.hamburg.pathfinder;
+
+/**
+ * The Pathfinder interface
+ * to be implemented by the pathfinder classes implementing
+ * the graph pathfinding algorithms, such as the Depth-First Search or Dijkstra's algorithm.
+ *
+ * @author Yuriel849
+ * @version 02.03.2021
+ */
+public interface Pathfinder {
+    // Graph graph
+
+    /**
+     * Finds the next step in the pathfinding process.
+     * Returns information on the next step to take, to pass on to the GUI.
+     */
+    // public Set findNextStep() {}
+}
