b0b52f7b9bdd09f096cc3c3887a617d73fc5d17b
Yuriel849 <yuriel849sp@gmail.com>
2021-03-01 23:57:44 +0100

    Spring project created

---
+++ src/main/java/edu/hamburg/controller/GraphPathfinderApplication.java
@@ -0,0 +1,13 @@
+package edu.hamburg.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphPathfinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphPathfinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hamburg/controller/GraphPathfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hamburg.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphPathfinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
