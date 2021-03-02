d61a381b4d1830372e05f81a95a1ca6c92557e68
Yuriel849 <yuriel849sp@gmail.com>
2021-03-02 16:41:14 +0100

    Edge class

--- src/main/java/edu/hamburg/model/Edge.java
+++ src/main/java/edu/hamburg/model/Edge.java
@@ -1,2 +1,13 @@
-package edu.hamburg.model;public class Edge {
+package edu.hamburg.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Edge {
+    // Destination vertex of this edge
+    private int dest;
+    // Weight of this edge
+    private int weight;
 }
