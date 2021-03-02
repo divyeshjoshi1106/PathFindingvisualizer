bf587e056baba0cf0382bf70e8934d85a50fe4a0
Yuriel849 <yuriel849sp@gmail.com>
2021-03-02 23:55:32 +0100

    Parameters edited

--- src/main/java/edu/hamburg/util/RandomGenerator.java
+++ src/main/java/edu/hamburg/util/RandomGenerator.java
@@ -12,14 +12,22 @@ import java.util.Set;
  * @version 02.03.2021
  */
 public class RandomGenerator {
-    public static Graph generateRandomGraph(int number, boolean weighted) {
+    /**
+     * Generates a random graph with the given number of vertices.
+     * The edges are weighted, if so designated.
+     * @param number The number of vertices in this graph.
+     * @param maxWeight The maximum weight of any edge.
+     * @param weighted Boolean designation of whether the edges are weighted or not.
+     * @return The generated random graph.
+     */
+    public static Graph generateRandomGraph(int number, int maxWeight, boolean weighted) {
         Graph graph = new Graph(number, weighted);
         Random random = new Random(System.currentTimeMillis());
 
         for(int i = 0; i < number * 2; i++) {
             int from, to, weight = 0;
             if(weighted)
-                weight = random.nextInt(20);
+                weight = random.nextInt(maxWeight-1) + 1; // To force a minimum weight of 1.
             from = random.nextInt(number);
             while((to = random.nextInt(number)) == from);
             graph.addEdge(from, to, weight);
@@ -29,7 +37,7 @@ public class RandomGenerator {
     }
 
     public static void main(String[] args) {
-        Graph graph = generateRandomGraph(10, true);
+        Graph graph = generateRandomGraph(10, 20, true);
 
         for(Set set : graph.getAdj()) {
             System.out.println(set.toString());
