818232b87822d2599f855c3b1015f8b26f9fc4da
Yuriel849 <yuriel849sp@gmail.com>
2021-03-06 22:53:18 +0100

    Add field 'pred', constructor and equals method

--- src/main/java/edu/hamburg/model/Vertex.java
+++ src/main/java/edu/hamburg/model/Vertex.java
@@ -10,10 +10,25 @@ import lombok.Data;
  * @version 05.03.2021
  */
 @Data
-@AllArgsConstructor
 public class Vertex {
     // Name (designation) of this vertex
     private String name;
+    // Predecessor vertex on the shortest path from the source to this vertex
+    private Vertex pred = null;
     // Distance from the starting vertex
     private int dist;
+
+    public Vertex(String name, int dist) {
+        this.name = name;
+        this.dist = dist;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Vertex other = (Vertex) object;
+        if(this.name.equals(other.getName()))
+            if(this.dist == other.getDist())
+            return true;
+        return false;
+    }
 }
