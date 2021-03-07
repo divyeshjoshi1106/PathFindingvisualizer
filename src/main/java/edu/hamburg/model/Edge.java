1e18b7e93bdd4ce57617da7b60cc81f19c90c3da
divshady <divyeshjoshi1106@gmail.com>
2021-03-07 13:47:45 +0100

    changes

--- src/main/java/edu/hamburg/model/Edge.java
+++ src/main/java/edu/hamburg/model/Edge.java
@@ -6,8 +6,15 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class Edge {
-    // Destination vertex of this edge
-    private String dest;
+    // Destination vertex & source of this edge
+    private String dest,src;
     // Weight of this edge
     private int weight;
+
+    Edge(String dest, String src, int weight)
+    {
+        this.dest = dest;
+        this.src = src;
+        this.weight = weight;
+    }
 }
