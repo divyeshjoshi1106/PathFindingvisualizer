7659ae6d87c9691f39c70c56338f59f72102c121
Yuriel849 <yuriel849sp@gmail.com>
2021-03-07 16:12:43 +0100

    Fix conflict and update lombok annotations

--- src/main/java/edu/hamburg/model/Edge.java
+++ src/main/java/edu/hamburg/model/Edge.java
@@ -2,30 +2,22 @@ package edu.hamburg.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 /**
  * The Edge class
  *
  * @author Yuriel849
- * @version 05.03.2021
+ * @version 07.03.2021
  */
 @Data
+@RequiredArgsConstructor
 @AllArgsConstructor
 public class Edge {
-<<<<<<< HEAD
-    // Destination vertex & source of this edge
-    private String dest,src;
-=======
     // Name (designation) of the destination vertex of this edge
-    private String dest;
->>>>>>> ec9dd9774e88c3ef9e6dea94cd0c3135e4d4132d
+    @NonNull private String dest;
+    private String src;
     // Weight of this edge
-    private int weight;
-
-    Edge(String dest, String src, int weight)
-    {
-        this.dest = dest;
-        this.src = src;
-        this.weight = weight;
-    }
+    @NonNull private int weight;
 }
