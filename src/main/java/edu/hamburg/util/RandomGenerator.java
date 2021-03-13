55acd660c0d40c4c5511e6165a936b8e6c1c9ebd
Yuriel849 <yuriel849sp@gmail.com>
2021-03-13 16:52:54 +0100

    Add autogeneration of Vertex names if names not given

--- src/main/java/edu/hamburg/util/RandomGenerator.java
+++ src/main/java/edu/hamburg/util/RandomGenerator.java
@@ -11,6 +11,12 @@ import java.util.*;
  * @version 02.03.2021
  */
 public class RandomGenerator {
+    static Random random = new Random(System.currentTimeMillis());
+
+    public static Graph generateRandomGraph(int number, int maxWeight) {
+        return generateRandomGraph(number, maxWeight, null);
+    }
+
     /**
      * Generates a random graph with the given number of vertices.
      * The edges are weighted, if so designated.
@@ -19,8 +25,17 @@ public class RandomGenerator {
      * @return The generated random graph.
      */
     public static Graph generateRandomGraph(int number, int maxWeight, List<String> names) {
+        // If the list of vertex names is not provided, autogenerate names for each vertex.
+        if(names == null) {
+            names = new ArrayList<>(number);
+            for(int i = 0; i < number; i++) {
+                char letter = (char) ('A' + i);
+                int digit = random.nextInt(number - 1) + 1; // To force a minimum value of 1.
+                names.add("" + letter + digit);
+            }
+        }
+
         Graph graph = new Graph(number, names);
-        Random random = new Random(System.currentTimeMillis());
 
         for(int i = 0; i < number * 2; i++) {
             String from, to;
@@ -36,7 +51,7 @@ public class RandomGenerator {
     public static void main(String[] args) {
         String[] array = { "A1", "B1", "B2", "D3", "D5", "E10", "E4", "F2", "G1", "H2" };
         ArrayList<String> names = new ArrayList(Arrays.asList(array));
-        Graph graph = generateRandomGraph(10, 20, names);
+        Graph graph = generateRandomGraph(10, 20);
 
         Map map = graph.getAdj();
         for(Object key : map.keySet()) {
