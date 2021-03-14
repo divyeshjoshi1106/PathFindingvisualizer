3962f143ec44a746eda9f73faa77c8edae45b66a
Yuriel849 <yuriel849sp@gmail.com>
2021-03-14 16:25:38 +0100

    Update BFSpathFinder to implement Pathfinder

--- src/main/java/edu/hamburg/pathfinder/BreadthFirst.java
+++ src/main/java/edu/hamburg/pathfinder/BreadthFirst.java
@@ -1,35 +1,33 @@
 package edu.hamburg.pathfinder;
 
+import edu.hamburg.model.Graph;
+import edu.hamburg.model.Vertex;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
-public class BFSpathFinder {
-    // function to form edge between two vertices
-    // source and dest
-    private static void addEdge(ArrayList<ArrayList<Integer>> adj, int i, int j)
-    {
-        adj.get(i).add(j);
-        adj.get(j).add(i);
-    }
-    private static void printShortestDistance(
-            ArrayList<ArrayList<Integer>> adj,
-            int s, int dest, int v)
-    {
-        // predecessor[i] array stores predecessor of
-        // i and distance array stores distance of i
-        // from s
-        int[] pred = new int[v];
-        int[] dist = new int[v];
+public class BreadthFirst implements Pathfinder {
+    @Override
+    public List findPath(Graph graph, int number, String start, String dest) {
+        // adj == graph, s == start, dest == dest, v == number, maxWeight = 0
 
-        if (!BFS(adj, s, dest, v, pred, dist)) {
-            System.out.println("Given source and destination" +
-                    "are not connected");
-            return;
+        ArrayList<String> indices = new ArrayList<>(number);
+        int[] pred = new int[number]; // Index 'i' of pred stores predecessor of i
+        int[] dist = new int[number]; // Index 'i' of dist stores distance of i from start
+
+        for(Vertex vertex : graph.getAdj().keySet()) { // Initialize the String array indices
+            indices.add(vertex.getName());
+        }
+
+        if (!BFS(graph, start, dest, number, pred, dist)) {
+            System.out.println("Given source and destination are not connected");
+            return null;
         }
 
         // LinkedList to store path
         LinkedList<Integer> path = new LinkedList<>();
-        int crawl = dest;
+        int crawl = indices.indexOf(dest);
         path.add(crawl);
         while (pred[crawl] != -1) {
             path.add(pred[crawl]);
@@ -37,14 +35,17 @@ public class BFSpathFinder {
         }
 
         // Print distance
-        System.out.println("Shortest path length is: " + dist[dest]);
+        System.out.println("Shortest path length is: " + dist[indices.indexOf(dest)]);
 
         // Print path
         System.out.println("Path is ::");
         for (int i = path.size() - 1; i >= 0; i--) {
             System.out.print(path.get(i) + " ");
         }
+
+        return null;
     }
+
 
     // a modified version of BFS that stores predecessor
     // of each vertex in array pred
@@ -99,31 +100,31 @@ public class BFSpathFinder {
     }
     public static void main(String[] args)
     {
-        // No of vertices
-        int v = 8;
-
-        // Adjacency list for storing which vertices are connected
-        ArrayList<ArrayList<Integer>> adj =
-                new ArrayList<ArrayList<Integer>>(v);
-        for (int i = 0; i < v; i++) {
-            adj.add(new ArrayList<Integer>());
-        }
-
-        // Creating graph given in the above diagram.
-        // add_edge function takes adjacency list, source
-        // and destination vertex as argument and forms
-        // an edge between them.
-        addEdge(adj, 0, 1);
-        addEdge(adj, 0, 3);
-        addEdge(adj, 1, 2);
-        addEdge(adj, 3, 4);
-        addEdge(adj, 3, 7);
-        addEdge(adj, 4, 5);
-        addEdge(adj, 4, 6);
-        addEdge(adj, 4, 7);
-        addEdge(adj, 5, 6);
-        addEdge(adj, 6, 7);
-        int source = 0, dest = 7;
+//        // No of vertices
+//        int v = 8;
+//
+//        // Adjacency list for storing which vertices are connected
+//        ArrayList<ArrayList<Integer>> adj =
+//                new ArrayList<ArrayList<Integer>>(v);
+//        for (int i = 0; i < v; i++) {
+//            adj.add(new ArrayList<Integer>());
+//        }
+//
+//        // Creating graph given in the above diagram.
+//        // add_edge function takes adjacency list, source
+//        // and destination vertex as argument and forms
+//        // an edge between them.
+//        addEdge(adj, 0, 1);
+//        addEdge(adj, 0, 3);
+//        addEdge(adj, 1, 2);
+//        addEdge(adj, 3, 4);
+//        addEdge(adj, 3, 7);
+//        addEdge(adj, 4, 5);
+//        addEdge(adj, 4, 6);
+//        addEdge(adj, 4, 7);
+//        addEdge(adj, 5, 6);
+//        addEdge(adj, 6, 7);
+//        int source = 0, dest = 7;
         printShortestDistance(adj, source, dest, v);
     }
 }
